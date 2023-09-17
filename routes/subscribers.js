08247f5b4f75964ea6352972f013668423cce7d4
Alex Lin <linpeixi@gmail.com>
2023-09-17 15:53:18 -0400

    added get subscriber method, implemented get one specific route

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const Subscriber = require('../models/subscriber')
+const subscriber = require("../models/subscriber")
 // Get all routes
 router.get('/', async (req, res) => {
     try {
@@ -13,13 +14,25 @@ router.get('/', async (req, res) => {
 })
 
 // Get one route
-router.get('/:id', (req, res) => {
-    res.send(id)
+router.get('/:id', getSubscriber, (req, res) => {
+    res.send(res.subscriber.name)
 })
 
 // Create one route
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save()
 
+        // status 200 means successful
+        // status 201 is more detailed successful regarding creation
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 })
 
 
@@ -32,4 +45,21 @@ router.patch('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
 
 })
+
+async function getSubscriber(req, res, next) {
+    let subscriber
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            // status 404 = not found
+            return res.status(404).json({ message: 'Cannot find subscriber '}) 
+        }
+    } catch (err) {
+        return res.status(500).json({ messgae: res.message })
+    }
+
+    res.subscriber = subscriber
+    next()
+}
+
 module.exports = router
